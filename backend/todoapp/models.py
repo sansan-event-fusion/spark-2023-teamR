2bb224ff4709fedbf36d333aae12ed8d464c3c5a
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-09-09 16:29:33 +0900

    モデルのうち、Position と Company を作成

--- backend/todoapp/models.py
+++ backend/todoapp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    # Enum のように 3 段階にする
+    # https://qiita.com/shun198/items/5e29f512139078c63efd
+    class PositionChoices(models.TextChoices):
+        POSITION_NEW_GRADUATE = "new_graduate"
+        POSITION_DIRECT_SUPERVISOR = "direct_supervisor"
+        POSITION_MANAGER = "manager"
+
+    position = models.CharField(
+        max_length=20,
+        choices=PositionChoices.choices,
+        default=PositionChoices.POSITION_NEW_GRADUATE,
+    )
+
+    def __str__(self):
+        return self.position
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        # オブジェクトがまだデータベースに保存されていない場合、パスワードをハッシュ化する
+        if not self.pk:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
+class Folder(models.Model):
+    pass
+
+
+class Task(models.Model):
+    pass
+
+
+class Emotion(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass

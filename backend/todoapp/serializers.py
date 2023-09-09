6910d19002f4505a7aa6cd1fa870ed6644aca190
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-09-09 20:08:09 +0900

    signup のためのルーティング・ビューを作成

---
+++ backend/todoapp/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from .models import CustomUser, Company
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(write_only=True)
+    company_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "id",
+            "username",
+            "password",
+            "email",
+            "company_id",
+            "position_id",
+            "company_name",
+            "company_password",
+        )
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+    def create(self, validated_data):
+        company_name = validated_data.pop("company_name")
+        company_password = validated_data.pop("company_password")
+
+        try:
+            company = Company.objects.get(name=company_name)
+            if not company.password == make_password(company_password):
+                raise serializers.ValidationError("Company password does not match.")
+            validated_data["company_id"] = company
+        except Company.DoesNotExist:
+            raise serializers.ValidationError("Company does not exist.")
+
+        user = CustomUser.objects.create(**validated_data)
+        return user

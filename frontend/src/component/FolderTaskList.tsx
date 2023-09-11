450cd740de77ec210a9b8182162c34f7abcb71bd
TatsuyaHasunuma0123 <thasu.work@gmail.com>
2023-09-11 20:18:43 +0900

    add: folder追加時のPopover

--- frontend/src/component/FolderTaskList.tsx
+++ frontend/src/component/FolderTaskList.tsx
@@ -1,18 +1,34 @@
-import React from "react";
 import { useState } from "react";
-import { Flex, Button, Input } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  FormLabel,
+  FormErrorMessage,
+  FormControl,
+} from "@chakra-ui/react";
 import { AddIcon, Icon } from "@chakra-ui/icons";
 import TaskList from "./TaskList";
 
 function FolderTaskList() {
   const [buttonBeforeIndex, setButtonBeforeIndex] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [folderName, setFolderName] = useState("");
+  const [vision, setVision] = useState("");
+  const [error, setError] = useState("");
   const [buttonStates, setButtonStates] = useState([
-    { name: "ボタン1", active: true },
-    { name: "ボタン2", active: false },
-    { name: "ボタン3", active: false },
+    { folderName: "ボタン1", vision: "ボタン1のビジョン", active: true },
+    { folderName: "ボタン2", vision: "ボタン2のビジョン", active: false },
+    { folderName: "ボタン3", vision: "ボタン3のビジョン", active: false },
   ]);
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
-  const [inputValue, setInputValue] = useState("");
 
   const handleFolderButtonClick = (index: number) => {
     const updatedButtonStates = [...buttonStates];
@@ -24,28 +40,31 @@ function FolderTaskList() {
   };
 
   const handleButtonClick = () => {
-    setIsButtonClicked(true);
+    setIsOpen(!isOpen);
   };
 
-  const handleInputChange = (e: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
-    setInputValue(e.target.value);
-  };
+  const handleSubmit = () => {
+    if (vision.trim() === "" || folderName.trim() === "") {
+      setError("入力必須です。空白は使用できません");
+    } else {
+      const newButtonState = {
+        folderName: folderName,
+        vision: vision,
+        active: false,
+      };
 
-  const handleFocus = () => {
-    setIsButtonClicked(false);
-    setInputValue("");
-  };
-
-  const handleEnterPress = (e: { key: string }) => {
-    if (e.key === "Enter" && inputValue.trim() !== "") {
-      const newButtonState = { name: inputValue, active: false };
       const updatedButtonStates = [...buttonStates, newButtonState];
       setButtonStates(updatedButtonStates);
-      setInputValue("");
-      setIsButtonClicked(false);
+      setVision("");
+      setFolderName("");
+      onClose();
     }
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+    setError("");
+    setError("");
   };
 
   return (
@@ -54,39 +73,77 @@ function FolderTaskList() {
         {buttonStates.map((button, index) => (
           <Button
             rounded="none"
-            key={button.name}
+            key={button.folderName}
             onClick={() => handleFolderButtonClick(index)}
             bg={button.active ? "blue.100" : "white"}
             size="lg"
           >
-            {button.name}
+            {button.folderName}
           </Button>
         ))}
-        {isButtonClicked ? (
-          <Input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            onKeyDown={handleEnterPress}
-            onBlur={handleFocus}
-            autoFocus
-          />
-        ) : (
-          <Button
-            bg={"white"}
-            p={4}
-            _hover={{
-              bg: "gray.100",
-              ".rotate-icon": {
-                transform: "rotate(90deg)",
-                transition: "transform 0.3s",
-              },
-            }}
-            onClick={handleButtonClick}
-          >
-            <Icon as={AddIcon} className="rotate-icon" />
-          </Button>
-        )}
+        <Popover placement="right" isOpen={isOpen}>
+          <PopoverTrigger>
+            <Button
+              bg={"white"}
+              p={4}
+              _hover={{
+                bg: "gray.100",
+                ".rotate-icon": {
+                  transform: "rotate(90deg)",
+                  transition: "transform 0.3s",
+                },
+              }}
+              onClick={handleButtonClick}
+            >
+              <Icon as={AddIcon} className="rotate-icon" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent width="auto" right={0} marginTop="5px">
+            <PopoverArrow />
+            <PopoverCloseButton onClick={onClose} />
+            <PopoverHeader>
+              <center>フォルダの追加</center>
+            </PopoverHeader>
+
+            <PopoverBody marginY="2">
+              <FormControl isInvalid={Boolean(error)}>
+                <FormLabel w="60vh" marginX="2" marginY="2">
+                  フォルダ名
+                </FormLabel>
+                <Input
+                  value={folderName}
+                  onChange={(e) => setFolderName(e.target.value)}
+                />
+                <FormErrorMessage>{error}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={Boolean(error)}>
+                <FormLabel w="60vh" marginX="2" marginY="2">
+                  ビジョン
+                </FormLabel>
+                <Input
+                  id="vision"
+                  value={vision}
+                  onChange={(e) => setVision(e.target.value)}
+                  isInvalid={vision.length === 0}
+                />
+                <FormErrorMessage>{error}</FormErrorMessage>
+              </FormControl>
+            </PopoverBody>
+            <PopoverFooter border="none">
+              <Flex justifyContent="flex-end">
+                <Button
+                  colorScheme="blue"
+                  type="submit"
+                  mr={3}
+                  onClick={handleSubmit}
+                  mb="4"
+                >
+                  追加
+                </Button>
+              </Flex>
+            </PopoverFooter>
+          </PopoverContent>
+        </Popover>
       </Flex>
 
       <TaskList buttonStates={buttonStates} />

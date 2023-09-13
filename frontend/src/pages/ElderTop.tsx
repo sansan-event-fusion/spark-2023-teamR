c27bffcc16e6043f23ff22cb20b77b1936e692de
riko-okubo <12riko14@gmail.com>
2023-09-13 12:29:33 +0900

    途中

--- frontend/src/pages/ElderTop.tsx
+++ frontend/src/pages/ElderTop.tsx
@@ -1,11 +1,22 @@
-import { Logout } from "./Logout";
+import LeftPlacement from "../component/LeftPlacement";
+import { FresherProfile } from "../component/fresher_profile/FresherProfile";
+import { TaskList } from "../component/TaskList";
+import { Box, Center, HStack, VStack } from "@chakra-ui/react";
 
 const ElderTop = () => {
   return (
-    <>
-      ElderTop
-      <Logout />
-    </>
+    <HStack>
+      <LeftPlacement />
+
+      <VStack w={"100%"} h={"100%"} marginLeft={"80px"}>
+        <Box bg={"black"} w="auto" p={2}>
+          <FresherProfile />
+        </Box>
+        <Box w="80%" h="80vh">
+          <TaskList />
+        </Box>
+      </VStack>
+    </HStack>
   );
 };
 
